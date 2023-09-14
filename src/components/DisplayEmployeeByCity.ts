19f47852e508f07a93bb32360ebd230da44d8505
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-14 12:06:11 +0200

    finished

--- src/components/DisplayEmployeeByCity.ts
+++ src/components/DisplayEmployeeByCity.ts
@@ -2,10 +2,10 @@ import employees from '../data/employees.json';
 
 export const DisplayEmployeeByCity = (city: string) => {
 	return /* html */ `
-		<h2>Employes who live in ${city}</h2>
+		<h2>Employees who live in ${city}</h2>
 
-		${employees.filter(m => m.address.city === city).map(employee => {
-			return `<div>${employee.firstName} ${employee.lastName} (${employee.address.city})</div>`
+		${employees.filter(m => m.address.city === city).map((employee, index, arr) => {
+			return `<div class="employee_id_${index}">${employee.firstName} ${employee.lastName} (${employee.address.city}) [employee ${index+1} of ${arr.length}]</div>`
 		}).join('')}
 	`;
 }
